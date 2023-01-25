5ef24301a509c7d6467df039f88f14d17b110104
Guilherme Santos <guilhermesantos12.ti@gmail.com>
2023-01-25 16:22:57 -0300

    Primeiro Commit

---
+++ src/pages/FormStep1/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+import { Theme } from '../../components/Theme';
+
+export const FormStep1 = () => {
+    return (
+        <C.Container>
+            Passo 1
+        </C.Container>
+    );
+}
